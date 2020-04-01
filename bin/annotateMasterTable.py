25237defc5e68490ffbbae3dda0958960a4c417d
daniel.malzl <daniel.malzl@imp.ac.at>
2020-04-01 17:06:38 +0200

    changes to processing flow

--- bin/annotateMasterTable.py
+++ bin/annotateMasterTable.py
@@ -16,34 +16,20 @@ class PromoterAnnotator():
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-    parser = ap.ArgumentParser(description='''combines the given tables to a master table containing all information,
-    if --foldchange is set, an additional column is inserted into the table denoting if the peak is up- or downregulated
-    or unchanged with respect to the given column names and a fold change threshold i.e. if the second column value is larger
-    than fold change threshold + column1 the peak is upragulated (i.e. 1), if the first column value is smaller than 
-    fold change threshold + column2 than the peak is downregulated (i.e. -1) else unchanged (i.e. 0)''')
-    parser.add_argument('-ct', '--counttable', required = True,
-                        help = 'tab-separated table holding the readquantification information')
+    parser = ap.ArgumentParser()
+    parser.add_argument('-mt', '--masterTable', required = True,
+                        help = '''tab-separated table holding at least initiation site
+                                  coordinates and name (i.e. chr, start, end, name)''')
     parser.add_argument('-a', '--annotation', required = True,
                         help = 'entrez mapped ChIPseeker annotation table')
-    parser.add_argument('-o', '--out', required = True,
+    parser.add_argument('-o', '--outputFile', required = True,
                         help = 'name of the output file')
-    parser.add_argument('-f', '--fold', default = 1.5, type = float,
-                        help = 'threshold for foldchange for peak classification, is log2 transformed before application')
-    parser.add_argument('-wt', '--wtcolumn', default = 'CH12_shLacZ',
-                        help = 'name of the column in --counttable holding the WT counts ')
-    parser.add_argument('-kd', '--kdcolumn', default = 'CH12_shMcm6',
-                        help = 'name of the column in --counttable holding the KD counts ')
-    parser.add_argument('--dormantcutoff', default = 2, type = float,
-                        help = 'cutoff to use to define dormant origins')
-    parser.add_argument('--absentcutoff', default = 2, type = float,
-                        help = 'cutoff to use to define absent origins')
     args = parser.parse_args()
 
-    logging.info('reading counttable %s' % args.counttable)
-    counts = pd.read_csv(args.counttable, sep = '\t', header = None, skiprows = 1,
-                         names = ['chr', 'start', 'end', args.wtcolumn, args.kdcolumn])
-    counts.sort_values(by=['chr', 'start'], inplace = True)
-    counts.reset_index(drop = True, inplace = True)
+    logging.info('reading masterTable %s' % args.masterTable)
+    mt = pd.read_csv(args.masterTable, sep = '\t')
+    mt.sort_values(by=['chr', 'start'], inplace = True)
+    mt.reset_index(drop = True, inplace = True)
 
     logging.info('reading mapped annotation table %s' % args.annotation)
     anno = pd.read_csv(args.annotation, sep = '\t')
@@ -54,41 +40,8 @@ if __name__ == '__main__':
     anno.loc[:, 'annotation'] = anno.annotation.apply(unifyAnno)
 
     logging.info('merging tables')
-    anno = anno.merge(counts[['chr', 'start', 'end', args.wtcolumn, args.kdcolumn]],
-                      how = 'left', on = ['chr', 'start', 'end'])
+    mt = mt.merge(anno[['name', 'annotation', 'symbol'],
+                  how = 'left', on = ['chr', 'start', 'end'])
 
     logging.info('writing output table')
-
-    pseudo = any(anno[[args.wtcolumn, args.kdcolumn]] == 0)
-
-    if pseudo:
-        logging.info('zero counts detected adding pseudo count of 1')
-
-    for col in [args.wtcolumn, args.kdcolumn]:
-        anno.loc[anno[col] == 0, col] = 1
-        anno['log2' + col] = np.log2((anno[col]*1000000)/anno[col].sum())
-
-    # finding the different categories
-    fc = np.log2(args.fold)
-    anno['class'] = 0
-
-    # dormant
-    anno.loc[anno['log2' + args.wtcolumn] < anno[anno['log2' + args.kdcolumn] < args.dormantcutoff]['log2' + args.wtcolumn].min(), 'class'] = 1
-
-    # upregulated
-    anno.loc[(anno['class'] == 0) & (anno['log2' + args.kdcolumn] > fc + anno['log2' + args.wtcolumn]), 'class'] = 2
-
-    # downregulated
-    anno.loc[(anno['class'] == 0) & (anno['log2' + args.wtcolumn] > fc + anno['log2' + args.kdcolumn]), 'class'] = 4
-
-    # absent
-    anno.loc[anno['log2' + args.kdcolumn] < anno[anno['log2' + args.wtcolumn] < args.absentcutoff]['log2' + args.kdcolumn].min(), 'class'] = 5
-
-    # unchanged
-    anno.loc[anno['class'] == 0, 'class'] = 3
-
-
-    anno = anno.loc[:, ['chr', 'start', 'end', 'name', 'log2' + args.wtcolumn, 'log2' + args.kdcolumn,
-                        'class', 'annotation', 'symbol']]
-
-    anno.to_csv(args.out, sep = '\t', index = False)
+    mt.to_csv(args.outputFile, sep = '\t', index = False)
